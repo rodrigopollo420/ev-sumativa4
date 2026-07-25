2f8c11a167558fb8ba4445074d234f6e6ec29dd8
Diego <joaku.no.se.lol@gmail.com>
2026-07-25 00:28:01 -0400

    crear formulario para añadir comentarios

--- src/components/comentarios/ComentariosForm.jsx
+++ src/components/comentarios/ComentariosForm.jsx
@@ -1,8 +1,25 @@
-
+import { useState } from "react";
 
 function ComentariosForm({ onAdd }) {
+    const [autor, setAutor] = useState("");
+    const [contenido, setContenido] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAdd({ autor, contenido });
+        setAutor("");
+        setContenido("");
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
+            <label>Autor:
+                <input type="text" value={autor} onChange={(e) => setAutor(e.target.value)} />
+            </label>
+            <label>Comentario:
+                <textarea value={contenido} onChange={(e) => setContenido(e.target.value)}></textarea>
+            </label>
+            <button type="submit" >Comentar</button>
 
         </form>
     )
