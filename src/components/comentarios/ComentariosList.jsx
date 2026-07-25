f18f4959c33081f1fc031760fab807a5de885a6e
Diego <joaku.no.se.lol@gmail.com>
2026-07-25 19:21:44 -0400

    agregar edicion visible de comentario

--- src/components/comentarios/ComentariosList.jsx
+++ src/components/comentarios/ComentariosList.jsx
@@ -1,22 +1,55 @@
+import { useState } from "react";
 
+function ComentariosList({ comentarios,onEliminar, onEditar}) {
 
-function ComentariosList({ comentarios,onEliminar }) {
+    const [editandoId, setEditandoId] = useState(null);
+    const [textoEdicion, setTextoEdicion] = useState("");
+
+    const empezarEdicion = (comentario) => {
+        setEditandoId(comentario.id);
+        setTextoEdicion(comentario.contenido);
+    }
+
+    const guardarEdicion = (id) => {
+        onEditar(id, textoEdicion);
+        setEditandoId(null);
+    }
+
+    const cancelarEdicion = () => {
+        setEditandoId(null);
+        setTextoEdicion("");    
+    }
+
     return (
         <div>
             {comentarios.length === 0 && <p>Sin comentarios por ahora</p>}
 
-            {
-                comentarios.map(c => (
-                    <p key={c.id}>
-                        <strong>{c.autor}:</strong> {c.contenido}
+            {comentarios.map ((c) => (
+                <p key={c.id}>
+                    <strong>{c.autor}:</strong>{" "}
+                    {editandoId === c.id ? (
+                        <>
+                        
+                            <textarea 
+                                value={textoEdicion}
+                                onChange={(e) => setTextoEdicion(e.target.value)}
+                            />
+                            <button onClick={() => guardarEdicion(c.id) }>Guardar</button>
+                            <button onClick={cancelarEdicion}>Cancelar</button>
+                        </>
+                    ) :(
+                        <>
+                        {c.contenido}
+                        <button onClick={() => empezarEdicion(c)}>Editar</button>
                         <button onClick={() => onEliminar(c.id)}>Eliminar</button>
-                    </p>
-                )
-                )
-            }
+                        </>
+                    )}
+                </p>                
+            ))}
+
 
         </div>
-    )
+    );
 }
 
 export default ComentariosList;
