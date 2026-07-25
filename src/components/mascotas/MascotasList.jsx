f599e01a64bd9dc2a9e9177c2226a7381ba958fe
Rodrigo <rodrigo.mollo03@inacapmail.cl>
2026-07-25 01:53:02 -0400

    Selector de estado en MascotasList (Conectado al PATCH) añadido

--- src/components/mascotas/MascotasList.jsx
+++ src/components/mascotas/MascotasList.jsx
@@ -1,11 +1,23 @@
-
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MascotasForm from "./MascotasForm";
+import mascotasApi from "../../api/api";
 
 
-function MascotasList({ lista, onAdd, onDelete }) {
+function MascotasList({ lista, onAdd, onDelete, onUpdateEstado }) {
+    const [estados, setEstados] = useState([]);
+    const fetchEstados = async () => {
+        try {
+            const response = await mascotasApi.get("choices/");
+            setEstados(response.data.estado);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-
+    useEffect(() => {
+        fetchEstados();
+    }, [])
 
     return (
         <>
@@ -22,6 +34,16 @@ function MascotasList({ lista, onAdd, onDelete }) {
                         <p>{m.descripcion}</p>
                         <p>Edad: {m.edad}</p>
                         <p>Raza: {m.raza}</p>
+                        <label>Estado:
+                            <select 
+                                value={m.estado} 
+                                onChange={(e) => onUpdateEstado(m.id, e.target.value)}
+                            >
+                                {
+                                    estados.map(e => <option value={e.value} key={e.value}>{e.label}</option>)
+                                }
+                            </select>
+                        </label>
                         <Link to={`${m.id}`}>Ver mascota</Link>
                         <button onClick={() => onDelete(m.id)} >Eliminar</button>
 
