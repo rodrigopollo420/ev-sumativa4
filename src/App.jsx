b3b57e9270452ac05b29d7e02d8309f4e5c1cc14
Rodrigo <rodrigo.mollo03@inacapmail.cl>
2026-07-26 01:23:23 -0400

    Imports sin uso eliminados de App y main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import MascotasPage from "./pages/MascotasPage";
-import MascotasForm from "./components/mascotas/MascotasForm";
 import MascotasDetail from "./components/mascotas/MascotasDetail";
 
 function App() {
